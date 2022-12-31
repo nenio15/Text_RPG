17c7e4166ee5b1d3769d39042083653749608127
gaon_0 <89757642+nenio15@users.noreply.github.com>
2022-12-31 22:27:31 +0900

    stat source

--- Assets/Resource/script/CharacterManager.cs
+++ Assets/Resource/script/CharacterManager.cs
@@ -1,21 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class character
+[Serializable]
+public class Character
 {
-    int hp;
-    int mp;
-    int money;
-    int exp;
+    int Level = 1;
+    int[] Hp = { 3, 3 };
+    int[] Mp = { 0, 0 };
+    int[] Stat = { 1, 1, 1, 1, 1, 1 };
+    //int money;
+    //int exp;
 
-    char back;
+    string Name = "홍길동";
+    string Class = "Warrior";
+}
+// 퀘스트는 진행정도를 기록.. 필요..
+
+
+public class CharacterManager : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+
+
 }
 
-public class CharacterManager
-{
-   
-
-
-
-}
+//TextAsset jsonData = Resources.Load("Text/Battle/Player") as TextAsset;
+//var _data = JsonUtility.FromJson<Character>(jsonData.ToString());
+//var _dd = JsonUtility.ToJson(_data);
